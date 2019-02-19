e51714fb4698a4232fc43bec152dd2513212f1f5
Guillermo Cabrera <guillermo.cabrera@skyangel.com.mx>
2019-02-19 11:54:36 -0600

    Generacion de la estructura para la lista de tareas

---
+++ vue.js
@@ -0,0 +1,23 @@
+new Vue({
+    el: '#app',  
+    data:{
+        titulo: 'Lista de tareas',
+        tareas: [
+            {
+                texto: 'Aprender Vue.js',
+                terminada: false
+            },
+            {
+                texto: 'Aprender Angular 6',
+                terminada: false
+            },
+            {
+                texto: 'Aprender Ionic 2',
+                terminada: false
+            },
+        ]
+    },
+    methods:{
+
+    }  
+});
