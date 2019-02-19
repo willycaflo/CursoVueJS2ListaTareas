139e573c9128c61c946e6b2ac48f08374bcac508
Guillermo Cabrera <guillermo.cabrera@skyangel.com.mx>
2019-02-19 13:16:58 -0600

    Creacion de componente para agregara tareas

--- vue.js
+++ vue.js
@@ -1,3 +1,25 @@
+var data = {
+    tareas: [
+        {
+            texto: 'Aprender Vue.js',
+            terminada: false
+        },
+        {
+            texto: 'Aprender Angular 6',
+            terminada: false
+        },
+        {
+            texto: 'Aprender Ionic 2',
+            terminada: false
+        },
+        {
+            texto: 'Aprender React',
+            terminada: false
+        },
+    ],
+    nuevaTarea: '',
+};
+
 //Componente para desplegar el titulo
 Vue.component('titulo',{
     template: '<h2>{{ titulo }}</h2>',
@@ -6,31 +28,34 @@ Vue.component('titulo',{
     }
 });
 
-
+//Componente para agregar una nueva tarea
+Vue.component('nueva-tarea',{
+    template: `<div class="input-group">
+            <input type="text" placeholder="Escribe una nueva tarea" v-model="nuevaTarea" class="form-control" v-on:keyup.enter="agregarTarea()"/>
+            <span class="input-group-btn">
+                <button type="button" v-on:click="agregarTarea()" class="btn btn-primary">Agregar</button>
+            </span>
+        </div>`,
+    data: function(){
+        return data;
+    },
+    methods: {
+        agregarTarea: function(){
+            var texto = this.nuevaTarea.trim();
+            if(texto){
+                this.tareas.push({
+                    texto: texto,
+                    terminada: false
+                });
+            }
+            this.nuevaTarea = '';
+        },
+    }
+});
 
 var app = new Vue({
     el: '#app',  
-    data:{
-        tareas: [
-            {
-                texto: 'Aprender Vue.js',
-                terminada: false
-            },
-            {
-                texto: 'Aprender Angular 6',
-                terminada: false
-            },
-            {
-                texto: 'Aprender Ionic 2',
-                terminada: false
-            },
-            {
-                texto: 'Aprender React',
-                terminada: false
-            },
-        ],
-        nuevaTarea: '',
-    },
+    data: data,
     methods:{
         agregarTarea: function(){
             var texto = this.nuevaTarea.trim();
