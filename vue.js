888d47fee4b79d97a03a7b4503b151d41a6cd965
Guillermo Cabrera <guillermo.cabrera@skyangel.com.mx>
2019-02-19 12:54:10 -0600

    Borra una tarea de la lista

--- vue.js
+++ vue.js
@@ -32,6 +32,9 @@ var app = new Vue({
                 });
             }
             this.nuevaTarea = '';
+        },
+        borrar: function(indice){
+            this.tareas.splice(indice,1);
         }
     }  
 });
